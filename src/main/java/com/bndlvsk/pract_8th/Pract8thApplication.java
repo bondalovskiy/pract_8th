e55b8ebcc2c14600171d755036ea3d8866d1eca8
Roma Bondalovskiy <bondalovskiyroma@gmail.com>
2025-02-18 13:47:43 +0300

    initial commit

---
+++ src/main/java/com/bndlvsk/pract_8th/Pract8thApplication.java
@@ -0,0 +1,13 @@
+package com.bndlvsk.pract_8th;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pract8thApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pract8thApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bndlvsk/pract_8th/Pract8thApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bndlvsk.pract_8th;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pract8thApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
