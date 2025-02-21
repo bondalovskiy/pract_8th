b84185182f24154a5915c0695a875ddc67f91697
Roma Bondalovskiy <bondalovskiyroma@gmail.com>
2025-02-21 12:56:38 +0300

    microservices/order-service init application.yml port8082/ module in pom.xml

---
+++ order-service/src/test/java/com/bndlvsk/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bndlvsk.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
